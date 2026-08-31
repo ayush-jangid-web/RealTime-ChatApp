e05141832a98209037b3d81527a7bde9e825cbe4
ayush-jangid-web <ayushjangid072@gmail.com>
2026-08-31 15:21:59 +0530

    Add dockerfile and implement express server for serving static files

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,13 @@ import cors from 'cors'
 import {clerkMiddleware} from '@clerk/express'
 import dbconnect from './src/config/db.js';
 
+import fs from 'fs'
+import path from "path";
+
 const app = express();
+
+const publicdir = path.join(process.cwd(),"public");
+
 app.use(express.json());
 app.use(cors({
     origin:process.env.FRONTEND_URL,
@@ -20,6 +26,16 @@ app.get("/health",(req,res)=>{
         success:true
     })
 })
+
+if(fs.existsSync(publicdir)){
+
+    app.use(express.static(publicdir))
+
+    app.get("/{*any}",(req,res,next)=>{
+        res.sendFile(path.join(publicdir, "index.html"),(err)=> next(err))
+    })
+
+}
 
 app.get("/",(req,res)=>{
     res.send("Hello from backend")
